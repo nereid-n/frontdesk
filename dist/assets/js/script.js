845dda8c304ae5e8a0830620d1510c8540feb5d8
nereid <nas.kuleshova@gmail.com>
2018-11-27 10:09:27 +0300

    change menu

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -159,6 +159,24 @@ $(document).ready(function () {
   $('.menu-link').click(function () {
     $('.header__menu').toggleClass('header__menu_show');
   });
+
+  if (document.querySelector('.header__menu')) {
+    var items = document.querySelectorAll('.header__menu li a');
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      if (document.body.clientWidth >= 1200) {
+        if (item.clientWidth > 140) {
+          item.style.width = '140px';
+          item.setAttribute('title', item.innerHTML);
+        }
+      } else if (document.body.clientWidth >= 992) {
+        if (item.clientWidth > 120) {
+          item.style.width = '120px';
+          item.setAttribute('title', item.innerHTML);
+        }
+      }
+    }
+  }
 });
 $(document).ready(function () {
   $('.video-btn-js').click(function () {
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
   $('.menu-link').click(function() {
     $('.header__menu').toggleClass('header__menu_show');
   });
+
+  if (document.querySelector('.header__menu')) {
+    let items = document.querySelectorAll('.header__menu li a');
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      if (document.body.clientWidth >= 1200) {
+        if (item.clientWidth > 140) {
+            item.style.width = '140px';
+            item.setAttribute('title', item.innerHTML);
+        }
+      }
+      else if (document.body.clientWidth >= 992) {
+        if (item.clientWidth > 120) {
+          item.style.width = '120px';
+          item.setAttribute('title', item.innerHTML);
+        }
+      }
+    }
+  }
 });
